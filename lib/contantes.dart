7d0517a4207ebcf2b1056faba65412aeed1f846e
Samuel <samuelvgnl@gmail.com>
2020-06-02 01:19:26 +0200

    add constantes
    add constantes + base main.dart

---
+++ lib/contantes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kTextColor = Color(0xFF3D3935);
+const kMediumColor = Color(0xFF777572);
+const kPrimaryColor = Color(0xFF006341);
+const kSecondaryColor = Color(0xFFFFFFFF);
+const kBackgroundColor = Color(0xFFF7F7F7);
