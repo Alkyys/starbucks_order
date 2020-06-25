e15017805e4b45db7f36bb0e4bf51f4855b871fa
Samuel <samuelvgnl@gmail.com>
2020-06-25 17:25:22 +0200

    creat form for number

--- lib/screens/input_number.dart
+++ lib/screens/input_number.dart
@@ -9,49 +9,89 @@ class InputNumber extends StatelessWidget {
     return Scaffold(
         backgroundColor: kPrimaryColor,
         resizeToAvoidBottomPadding: false,
-        appBar: AppBar(actions: <Widget>[
-          IconButton(
-            icon: SvgPicture.asset('assets/icon/help-circle.svg',
-                semanticsLabel: 'Help Logo'),
-            tooltip: 'Help',
-            onPressed: () {},
-          ),
-        ], iconTheme: IconThemeData(color: kBackgroundColor), elevation: 0),
-        body: Column(
-          children: <Widget>[
-            SizedBox(height: 75),
-            Center(
-                child: Text('Mon numero est',
-                    style: TextStyle(
-                      color: kBackgroundColor,
-                      fontSize: 35,
-                    ))),
-            SizedBox(height: 20),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 30),
-              child: Center(
-                child: TextFormField(
-                  keyboardType: TextInputType.number,
-                  inputFormatters: <TextInputFormatter>[
-                    WhitelistingTextInputFormatter.digitsOnly
-                  ],
-                  decoration: InputDecoration(
-                    enabledBorder: const OutlineInputBorder(
-                      borderSide:
-                          const BorderSide(color: kBackgroundColor, width: 3.0),
-                    ),
-                    border: const OutlineInputBorder(),
-                  ),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Please enter some text';
-                    }
-                    return null;
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(56),
+          child: Builder(
+            builder: (context) => AppBar(actions: <Widget>[
+              IconButton(
+                  tooltip: 'Help',
+                  onPressed: () {
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text('Help')));
                   },
-                ),
+                  icon: Container(
+                    child: SvgPicture.asset('assets/icon/help-circle.svg',
+                        semanticsLabel: 'Help Logo', fit: BoxFit.contain),
+                  )),
+            ], iconTheme: IconThemeData(color: kBackgroundColor), elevation: 0),
+          ),
+        ),
+        body: MyCustomForm());
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() {
+    return MyCustomFormState();
+  }
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  // Create a global key that uniquely identifies the Form widget
+  // and allows validation of the form.
+  //
+  // Note: This is a GlobalKey<FormState>,
+  // not a GlobalKey<MyCustomFormState>.
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          SizedBox(height: 75),
+          Center(
+              child: Text('Mon numero est',
+                  style: TextStyle(
+                    color: kBackgroundColor,
+                    fontSize: 35,
+                  ))),
+          SizedBox(height: 20),
+          new TextFormField(
+            keyboardType: TextInputType.number,
+            inputFormatters: <TextInputFormatter>[
+              WhitelistingTextInputFormatter.digitsOnly
+            ],
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                  icon: Icon(Icons.send),
+                  color: kBackgroundColor,
+                  onPressed: () {
+                    if (_formKey.currentState.validate()) {
+                      // If the form is valid, display a Snackbar.
+                      Scaffold.of(context).showSnackBar(
+                          SnackBar(content: Text('Processing Data')));
+                    }
+                  }),
+              enabledBorder: const OutlineInputBorder(
+                borderSide:
+                    const BorderSide(color: kBackgroundColor, width: 3.0),
               ),
+              border: const OutlineInputBorder(),
             ),
-          ],
-        ));
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'On dirait que tu as oublie quelque chose ...';
+              }
+              return null;
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
